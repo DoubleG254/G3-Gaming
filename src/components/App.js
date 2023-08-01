d8ecca40c80b5f228c263872819299c918ae7868
DoubleG254 <glorygweth6@gmail.com>
2023-08-01 20:42:02 +0300

    nav added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Home from "./Home";
+import NavBar from "./NavBar";
 function App() {
   return (
     <div>
+      <NavBar/>
       <Home/>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,24 @@
 import React from "react";
- 
+import logo from "./images/logo.png"
+import { NavLink } from "react-router-dom";
+
 function NavBar(){
      
     return(
-        <nav>
-            <div>
-                
-            </div>
-        </nav>
+        <div>
+            <header>
+                <img src={logo} alt="logo"/>
+                <h1>G3 Games</h1>
+            </header>
+            <nav>
+                <NavLink exact to="/">Home</NavLink>
+                <NavLink to="/games">Games</NavLink>
+                <NavLink to="/cart">Cart</NavLink>
+
+            </nav>
+        </div>
+        
     )
 }
+
+export default NavBar
